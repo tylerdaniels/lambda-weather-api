6106b3b9a4cf8889e9132122516f49eacfbc94e0
Brett Pyke <brett@brettpyke.com>
2024-09-22 14:46:11 +1000

    implement historical weather test

--- src/openweather/openweather-weather.service.spec.ts
+++ src/openweather/openweather-weather.service.spec.ts
@@ -1,4 +1,4 @@
-import { CurrentWeather } from '../types';
+import { CurrentWeather, HistoricalWeather } from '../types';
 import { mockFetchJson } from '../../test/utils';
 import { WeatherApi } from './openweather-weather.service';
 
@@ -38,5 +38,45 @@ describe('WeatherApi', () => {
     expect(params!.get('lon')).toBe(long.toString());
     expect(params!.get('units')).toBe(expected.units);
     expect(params!.get('appid')).toBe(key);
+  });
+  it('should call the historical weather api for the requested coordinates', async () => {
+    const key = 'apikey-123';
+    const lat = 40.71;
+    const long = -74.0;
+    const expected = {
+      units: 'metric',
+      '2020-03-04': {
+        temperatureMax: 25,
+        temperatureMin: 20,
+        rain: 12,
+      },
+    } as unknown as HistoricalWeather;
+    const [fetchFn, lastUri, lastQuery] = mockFetchJson({
+      lat,
+      lon: long,
+      tz: '+02:00',
+      date: '2020-03-04',
+      units: 'metric',
+      precipitation: {
+        total: expected['2020-03-04'].rain,
+      },
+      temperature: {
+        min: expected['2020-03-04'].temperatureMin,
+        max: expected['2020-03-04'].temperatureMax,
+      },
+    });
+    const api = new WeatherApi(key, fetchFn);
+    const results = await api.historical(lat, long);
+    expect(results).toEqual(expected);
+    expect(lastUri()).toBe(
+      'https://api.openweathermap.org/data/3.0/onecall/day_summary',
+    );
+    const params = lastQuery();
+    expect(params).toBeDefined();
+    expect(params!.get('lat')).toBe(lat.toString());
+    expect(params!.get('lon')).toBe(long.toString());
+    expect(params!.get('units')).toBe('metric');
+    expect(params!.get('appid')).toBe(key);
+    expect(params!.get('date')).toBeDefined();
   });
 });
